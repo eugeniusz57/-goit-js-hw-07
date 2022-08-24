25c10f992f6e35c755ff596f950e5de0b27e7ba7
eugeniusz57 <104994034+eugeniusz57@users.noreply.github.com>
2022-08-24 18:13:47 +0200

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,59 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from './gallery-items.js'
 // Change code below this line
+const divContainer = document.querySelector('.gallery');
 
-console.log(galleryItems);
+
+const makeGaleryMarkup = ({preview, original, description}) => {
+    return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `
+}
+
+//create markup galery
+const makeGaleryGreed = galleryItems.map(makeGaleryMarkup).join("");
+divContainer.innerHTML = makeGaleryGreed ;
+
+
+divContainer.addEventListener('click', onClickImg)
+
+function onClickImg(e){
+  onClickLink(e);
+
+  if(e.target.nodeName !== "IMG"){
+  return;
+  }
+  
+  const src = e.target.dataset.source;
+  const instance = basicLightbox.create(`
+      <img src="${src}" width="800" height="600">
+  `)
+
+  instance.show()
+//close modal window if click Esc
+  divContainer.addEventListener('keydown', onClickEsc)
+
+    function onClickEsc(e){
+      if(e.code === "Escape"){
+        instance.close()
+      }
+    }
+}
+
+function onClickLink(e){ 
+  e.preventDefault(); 
+}
+
+
+
+
+
+

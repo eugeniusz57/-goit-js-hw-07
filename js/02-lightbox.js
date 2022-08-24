4eefb05fad12129af9b168ad6de144ad40508382
eugeniusz57 <104994034+eugeniusz57@users.noreply.github.com>
2022-08-24 20:09:34 +0200

    Create task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const divContainer = document.querySelector('.gallery');
 
-console.log(galleryItems);
+
+const makeGaleryMarkup = ({preview, original, description}) => {
+    return `
+    <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}"  alt="${description}" />
+    </a>
+
+    `
+}
+
+//create markup galery
+const makeGaleryGreed = galleryItems.map(makeGaleryMarkup).join("");
+divContainer.innerHTML = makeGaleryGreed ;
+
+//use SimpleLightbox
+let lightbox = new SimpleLightbox('.gallery a', {   
+    captionsData:	'alt',
+     captionDelay:	250 ,
+    });
